ab95f0002514105f81f8738e831b9f25877ab330
Ravi Kalia <ravkalia@gmail.com>
2018-02-16 15:24:33 -0600

    finished iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,10 +5,15 @@ const Celebrity = require('../models/celebrity')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/Celebrities')
 
+// helpers to be moved to own module
 
-const handlerConstructor = (req,res,next) =>{
+const listCelebs = (req, res, next) => {
+  const allRender = allRenderConstructor(req,res,next)
+  Celebrity.find({}, allRender)
+} 
+
+const allRenderConstructor = (req,res,next) =>{
   return (err,data) => {
-    console.log(data);
     err ? (() => { next(); return err})()
       : res.render('celebrities/index', { 
         celebs: data, 
@@ -18,12 +23,31 @@ const handlerConstructor = (req,res,next) =>{
 }
 
 
-const celebGetHandler = (req, res, next) => {
-  const findThenRender = handlerConstructor(req,res,next)
-  Celebrity.find({}, findThenRender)
-} 
+const showCeleb = (req, res, next) =>{
+  const  oneRender = oneRenderConstructor(req,res,next)
+  Celebrity.findById(req.params.id, oneRender)
+
+}
+
+const oneRenderConstructor = (req, res, next) =>{
+  return (err,data) => {
+    err ? (() => { next(); return err})()
+      : res.render('celebrities/show', { 
+        celeb: data, 
+        title: `This is the celeb called ${data.name}` 
+      })
+  }
+}
+
+
+//########### Routes
 
 /* GET home page. */
-router.get('/', celebGetHandler)
+
+router.get('/', listCelebs)
+
+// ############## Show one Celeb
+router.get('/:id', showCeleb)
+
 
 module.exports = router
